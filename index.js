c2b784a3d088a8943a25d69ee607bd0293413a44
Adarsh Sharma <sharma007adarsh@gmail.com>
2023-05-23 14:37:52 +0530

    added middlewares

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const app = express();
 
 const PORT = 1337;
 
-
+//middlewares
+app.use(express.json());
+app.use(express.static("public"));
+app.use(express.urlencoded({extended: false}));
 
 app.listen(PORT, () => {
     console.log("The app is running at PORT = ",PORT);
