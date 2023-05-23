2dc2cb0e027eff22c40a4f16f802345982681766
Adarsh Sharma <sharma007adarsh@gmail.com>
2023-05-23 22:58:42 +0530

    database created

--- routes/url.js
+++ routes/url.js
@@ -4,7 +4,10 @@ const router = express.Router();
 router.post('/', async(req, res) => {
     try{
         const {longUrl} = req.body;
+        
+        const shortId = nanoid(4);
     } catch(e){
         console.error(e);
+        return res.status(500).send(e);
     }
 });
