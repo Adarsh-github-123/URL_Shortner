3122b8b1ce0935cb145d500e293862984b191e8c
Adarsh Sharma <sharma007adarsh@gmail.com>
2023-05-23 15:08:32 +0530

    handled the short url html and its api call

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require(express);
 const app = express();
+const homeRoutes = require("./routes/home");
+const shortUrl = require("./routes/url");
 
 const PORT = 1337;
 
@@ -7,6 +9,9 @@ const PORT = 1337;
 app.use(express.json());
 app.use(express.static("public"));
 app.use(express.urlencoded({extended: false}));
+
+app.use('/urlapi',shortUrl);
+app.use('/',homeRoutes);
 
 app.listen(PORT, () => {
     console.log("The app is running at PORT = ",PORT);
