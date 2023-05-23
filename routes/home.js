421404dc65fc7f2451ec9f8a2f053c2bc437a880
Adarsh Sharma <sharma007adarsh@gmail.com>
2023-05-23 16:00:34 +0530

    home.js updated

---
+++ routes/home.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+router.get('/', async(req, res) => {
+    const htmlPath = path.join(__dirname, 'public', 'index.html');
+    res.sendFile(htmlPath);
+});
+
+module.exports = router;
