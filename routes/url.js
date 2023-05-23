4cb4482c0ea5c98eccff77e022e99843f1d173bd
Adarsh Sharma <sharma007adarsh@gmail.com>
2023-05-23 16:39:49 +0530

    url.js is modified

---
+++ routes/url.js
@@ -0,0 +1,10 @@
+const express = require(express);
+const router = express.Router();
+
+router.post('/', async(req, res) => {
+    try{
+        const {longUrl} = req.body;
+    } catch(e){
+        console.error(e);
+    }
+});
