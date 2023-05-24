b3f602e78e6d190fb73610e396753f4d4cec5ed7
Adarsh Sharma <sharma007adarsh@gmail.com>
2023-05-24 07:20:10 +0530

    url.js updated

--- routes/url.js
+++ routes/url.js
@@ -1,12 +1,30 @@
 const express = require(express);
 const router = express.Router();
+const createDB = require('../config/db');
+const Url = require('../models/urlModel');
 
+const baseUrl = "http://127.0.0.1:5500/urlapi"
+
+//connecting database
+createDB.sync.then( () => {
+    console.log("Database created");
+})
 
 router.post('/', async(req, res) => {
     try{
         const {longUrl} = req.body;
         
         const shortId = nanoid(4);
+
+        const shortUrl = await Url.create({
+            longUrl,
+            shortUrl: shortId
+        });
+
+        return res.status(200).json({
+            status: "ok",
+            shortUrl: `${baseUrl}/${shortId}`
+        })
     } catch(e){
         console.error(e);
         return res.status(500).send(e);
